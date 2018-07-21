2e0a9b1f916188220210f788cf904d5ee6a029d4
mhaugrud@gmail.com <mhaugrud@gmail.com>
2018-07-21 09:43:07 -0500

    Signed-off-by: mhaugrud@gmail.com <mhaugrud@gmail.com>

--- pavement.py
+++ pavement.py
@@ -19,7 +19,7 @@ except ImportError:
 
 
 ######## CHANGE THIS ##########
-project_name = "haugrud"
+project_name = "msumoorhead"
 ###############################
 
 master_url = 'https://dev.runestone.academy'
